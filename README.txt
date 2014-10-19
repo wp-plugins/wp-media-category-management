9004890b81299256836a16c2be490ce02ccac007
DeBAAT <DeBAAT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-19 12:03:05 +0000

    Added tag for V1.1.0.
    git-svn-id: https://plugins.svn.wordpress.org/wp-media-category-management/tags/1.1.0@1009954 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,91 @@
+=== Plugin Name ===
+Contributors: DeBAAT
+Donate link: http://www.de-baat.nl/WP_MCM
+Tags: media library, bulk action, bulk toggle, toggle category, taxonomy, taxonomies, attachment, media category, media categories, media tag, media tags, media taxonomy, media taxonomies, media filter, media organizer, media types, media uploader, custom, media management, attachment management, files management, user experience, wp-admin, admin
+Requires at least: 4.0
+Tested up to: 4.0
+Stable tag: 1.1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+A plugin to provide bulk category management functionality for media in WordPress sites.
+
+== Description ==
+This WordPress plugin will ease the management of media categories, including bulk actions.
+It supports categories for media using either the existing post categories or a dedicated media_category custom taxonomy.
+The plugin supports easy category toggling on the media list page view and also bulk toggling for multiple media at once.
+
+* use post categories or dedicated media categories,
+* control your media categories via admin the same way as post categories,
+* bulk toggle any media taxonomy assignment from Media Library via admin,
+* filter media files in Media Library by your custom taxonomies, both in list and grid view
+* use new shortcode to filter the media on galleries in posts,
+
+== Installation ==
+
+1. Upload plugin folder to '/wp-content/plugins/' directory
+1. Activate the plugin through 'Plugins' menu in WordPress admin
+1. Adjust plugin's settings on **WP MCM -> Settings**
+1. Enjoy WordPress Media Category Management!
+1. Use shortcode `[wp_mcm category="<slug>"]` in your posts or pages, see also **WP MCM -> Shortcodes**
+
+== Frequently Asked Questions ==
+
+= How do I use this plugin? =
+
+On the options page, define which category to use for media: either use the standard post category or a dedicated media category.
+Define the categories to be used for media.
+Assign categories to media, either individually or in bulk.
+Use category filter when adding media to posts or pages.
+
+= How do I use the shortcode of this plugin? =
+
+Use the `[wp_mcm]` shortcode. Various shortcode uses are explained in the **WP MCM -> Shortcodes** page.
+
+== Screenshots ==
+
+1. The admin page showing the options for this plugin.
+2. Managing the new Media Category taxonomy.
+3. Setting Media Category options for a media post.
+4. Media List page view showing individual toggle options for first media post.
+5. Media List page view showing bulk toggle actions for selected media post.
+6. Media List page view showing filter options for Media Categories.
+7. Media Grid page view showing filter options for Media Categories.
+8. The admin page showing the shortcode explanations for this plugin.
+9. The post edit page showing an example using the [wp-mcm category="mediafoto,medialogo"] shortcode.
+10. The post page showing the results of the example using the [wp-mcm category="mediafoto,medialogo"] shortcode.
+
+== Changelog ==
+
+= 1.1.0 =
+* Create default options when activating.
+* Limit dark table header to WP_MCM shortcode screen only.
+
+= 1.0.0 =
+* Added category filter functionality when adding media to posts or pages.
+* Added functionality to define default category when adding or editing a media file.
+* Added a switch to enable and disable the assignment of default category.
+* Added category filter functionality to the media grid view.
+* Added a new screenshot showing filter in media grid view.
+
+= 0.2.0 =
+* Fixed bug managing post categories in combination with "Use Post Taxonomy" flag.
+* Improved asset images for repository.
+
+= 0.1.0 =
+* First version starting the plugin.
+
+== Upgrade Notice ==
+
+= 1.1.0 =
+* Added new functionality, see change log.
+
+= 1.0.0 =
+* Added new functionality, see change log.
+
+= 0.2.0 =
+* Fixed bug managing post categories in combination with "Use Post Taxonomy" flag.
+* Improved asset images for repository.
+
+= 0.1.0 =
+As this is the first version, there is no upgrade info yet.
---
+++ include/wp-mcm-functions.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * The WordPress Media Category Management Plugin.
+ *
+ * @package   WP_MediaCategoryManagement\Functions
+ * @author    De B.A.A.T. <wp-mcm@de-baat.nl>
+ * @license   GPL-3.0+
+ * @link      http://www.de-baat.nl/WP_MCM
+ * @copyright 2014 De B.A.A.T.
+ */
+
+
+function mcm_init_option_defaults() {
+	$wp_mcm_options = get_option(WP_MCM_OPTIONS_NAME);
+
+	// Always set the current version
+	$wp_mcm_options['wp_mcm_version'] = WP_MCM_VERSION;
+
+	// Only set defaults when the options are not set yet
+	if ( mcm_get_option('wp_mcm_toggle_assign') === false ) {
+		$wp_mcm_options['wp_mcm_toggle_assign']				= '1';
+		$wp_mcm_options['wp_mcm_use_post_taxonomy']			= '';
+		$wp_mcm_options['wp_mcm_use_default_category']		= '';
+		$wp_mcm_options['wp_mcm_default_media_category']	= '';
+		$wp_mcm_options['wp_mcm_default_post_category']		= '';
+	}
+
+	return update_option(WP_MCM_OPTIONS_NAME, $wp_mcm_options);
+}
+
+function mcm_get_option($option_key = '') {
+	$wp_mcm_options = get_option(WP_MCM_OPTIONS_NAME);
+	return isset( $wp_mcm_options[$option_key] ) ? $wp_mcm_options[$option_key] : false;
+}
+
+function mcm_update_option($option_key = '', $option_value = '') {
+	$wp_mcm_options = get_option(WP_MCM_OPTIONS_NAME);
+	if ( isset( $wp_mcm_options[$option_key] ) ) {
+		$wp_mcm_options[$option_key] = $option_value;
+	}
+	return update_option(WP_MCM_OPTIONS_NAME, $wp_mcm_options);
+}
+
+function mcm_get_option_bool($option_key = '') {
+	$wp_mcm_options = get_option(WP_MCM_OPTIONS_NAME);
+	if ( isset( $wp_mcm_options[$option_key] ) ) {
+		return ( mcm_string_to_bool( $wp_mcm_options[$option_key] ) );
+	} else {
+		return false;
+	}
+}
+
+function mcm_string_to_bool($value) {
+	if ($value == true || $value == 'true' || $value == 'TRUE' || $value == '1') {
+		return true;
+	}
+	else if ($value == false || $value == 'false' || $value == 'FALSE' || $value == '0') {
+		return false;
+	}
+	else {
+		return $value;
+	}
+}
+
+function mcm_get_media_taxonomy() {
+
+	// Check to use post taxonomy
+	if (mcm_string_to_bool(mcm_get_option('wp_mcm_use_post_taxonomy'))) {
+		return WP_MCM_POST_TAXONOMY;
+	}
+	/**
+	 * else:
+	 * Separate media categories from post categories
+	 * Use a custom category called 'category_media' for the categories in the media library
+	 */
+	return WP_MCM_MEDIA_TAXONOMY;
+}
+
+/** Custom update_count_callback */
+function mcm_get_category_ids( $mcm_atts = array() ) {
+
+	// Get media taxonomy and use default category value
+	$media_taxonomy = mcm_get_media_taxonomy();
+	if ($media_taxonomy == WP_MCM_POST_TAXONOMY) {
+		$media_categories = mcm_get_option( 'wp_mcm_default_post_category' );
+	} else {
+		$media_categories = mcm_get_option( 'wp_mcm_default_media_category' );
+	}
+
+	if (isset($mcm_atts['category']) && $mcm_atts['category'] != '') {
+		$media_categories = explode(',', $mcm_atts['category']);
+	}
+	if ( !is_array($media_categories)) {
+		$media_categories = array ( $media_categories );
+	}
+	mcm_debugMP('pr',__FUNCTION__ . ' taxonomy = ' . $media_taxonomy . ' categories = ', $media_categories);
+
+	// Get the posts associated with the media_taxonomy
+	$attachments_args = array(	'showposts' => -1,
+								'post_type' => 'attachment',
+								'post_parent' => null,
+								'tax_query' => array(
+									'relation' => 'OR',
+									array(
+//										'taxonomy' => $media_taxonomy,
+										'taxonomy' => WP_MCM_POST_TAXONOMY,
+										'field' => 'slug',
+										'terms' => $media_categories
+									),
+									array(
+//										'taxonomy' => $media_taxonomy,
+										'taxonomy' => WP_MCM_MEDIA_TAXONOMY,
+										'field' => 'slug',
+										'terms' => $media_categories
+									)
+								),
+	);
+
+	// Use gallery options if available
+	if (isset($mcm_atts['orderby']) && $mcm_atts['orderby'] != '') {
+		$attachments_args['orderby'] = $mcm_atts['orderby'];
+	}
+	if (isset($mcm_atts['order']) && $mcm_atts['order'] != '') {
+		$attachments_args['order'] = $mcm_atts['order'];
+	}
+
+	// Get the attachments for these arguments
+	$attachments = get_posts($attachments_args);
+	mcm_debugMP('pr',__FUNCTION__ . ' attachments found = ' . count($attachments) . ' with attachments_args = ', $attachments_args);
+
+	// Get the post IDs for the attachments found for POST
+	$attachment_ids = array();
+	if ( $attachments ) {
+		foreach ( $attachments as $post ) {
+			setup_postdata( $post );
+			$attachment_ids[] = $post->ID;
+		}
+		wp_reset_postdata();
+	}
+
+	$attachment_ids_result = implode(',', $attachment_ids);
+	mcm_debugMP('pr',__FUNCTION__ . ' attachment_ids_result = ' . $attachment_ids_result . ' attachment_ids = ', $attachment_ids);
+
+	return $attachment_ids_result;
+
+}
+
+/**
+ * Simplify the plugin debugMP interface.
+ *
+ * Typical start of function call: $this->debugMP('msg',__FUNCTION__);
+ *
+ * @param string $type
+ * @param string $hdr
+ * @param string $msg
+ */
+function mcm_debugMP($type,$hdr,$msg='') {
+
+	global $wp_mcm_plugin;
+	if (!is_object($wp_mcm_plugin)) { return; }
+
+	if (($type === 'msg') && ($msg!=='')) {
+		$msg = esc_html($msg);
+	}
+	if (($hdr!=='')) {
+		$hdr = 'Func:: ' . $hdr;
+	}
+
+	$wp_mcm_plugin->debugMP($type,$hdr,$msg,NULL,NULL,true);
+}
+
